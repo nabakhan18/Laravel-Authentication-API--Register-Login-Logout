6a5e7e435636abe0ee9bd1e0b7586e4b73464408
nabakhan18 <Nabazahid05@gmail.com>
2025-08-25 18:02:43 +0500

    Crud operations completed.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+// use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -17,10 +17,18 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+    //     'ref_id',
+    //     'full_name', // Changed from 'name'
+    //     'email',
+    //     'password',
+    //     'is_active',
+    // ];
     protected $fillable = [
-        'name',
+        'full_name',
         'email',
+        'ref_id',
         'password',
+        // 'is_active',
     ];
 
     /**
@@ -29,8 +37,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $hidden = [
+        // 'full_name',
+        // 'password',
+        // 'ref_id',
         'password',
-        'remember_token',
+        // 'is_active',
     ];
 
     /**
@@ -44,5 +55,21 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key-value array, containing any custom claims to be added to the JWT.
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,18 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('ref_id')->nullable();
+            $table->string('full_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestampsTz();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->timestampTz('created_at')->nullable();
+            $table->timestampTz('updated_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
